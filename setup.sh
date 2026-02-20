646bd1d4e387ef38ee992a65afc6c69cfc89b44f
hakspare <azizulislamshohan102@gmail.com>
2026-02-20 16:06:41 +0600

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,13 +1,22 @@
 #!/bin/bash
 
-# Detect OS
-if [ -d "/data/data/com.termux/files/usr/bin" ]; then
-    BIN_DIR="$PREFIX/bin"; OS="android"; ARCH="arm64"; SUDO=""
+# প্রসেসর ডিটেকশন (Fix for Exit Status 132)
+ARCH=$(uname -m)
+if [ "$ARCH" = "x86_64" ]; then
+    ARCH_TYPE="amd64"
+elif [[ "$ARCH" == "aarch64" || "$ARCH" == "arm64" ]]; then
+    ARCH_TYPE="arm64"
 else
-    BIN_DIR="/usr/bin"; OS="linux"; ARCH="amd64"; SUDO="sudo"
+    ARCH_TYPE="386"
 fi
 
-echo -e "\e[1;32m[*] Installing Dependencies...\e[0m"
+if [ -d "/data/data/com.termux/files/usr/bin" ]; then
+    BIN_DIR="$PREFIX/bin"; OS="android"; SUDO=""
+else
+    BIN_DIR="/usr/bin"; OS="linux"; SUDO="sudo"
+fi
+
+echo -e "\e[1;32m[*] Installing Dependencies for $ARCH_TYPE...\e[0m"
 if [ "$OS" = "android" ]; then
     pkg install curl unzip tar python python-pip -y
     pip install requests
@@ -23,17 +32,20 @@ install_tool() {
     else
         curl -sL "$url" -o "${name}.tar.gz" && tar -xzf "${name}.tar.gz" && rm "${name}.tar.gz"
     fi
+    # বাইনারি ফাইল খুঁজে সঠিক জায়গায় মুভ করা
     find . -maxdepth 1 -type f -name "$name*" -not -name "*.md" -exec $SUDO mv {} $BIN_DIR/$name \;
     $SUDO chmod +x $BIN_DIR/$name
 }
 
-echo -e "\e[1;33m[*] Downloading Binaries...\e[0m"
-install_tool "subfinder" "https://github.com/projectdiscovery/subfinder/releases/download/v2.6.6/subfinder_2.6.6_${OS}_${ARCH}.zip" &
-install_tool "gau" "https://github.com/lc/gau/releases/download/v2.2.3/gau_2.2.3_${OS}_${ARCH}.tar.gz" &
+echo -e "\e[1;33m[*] Downloading Correct Binaries for your CPU...\e[0m"
+# Subfinder installation
+install_tool "subfinder" "https://github.com/projectdiscovery/subfinder/releases/download/v2.6.6/subfinder_2.6.6_linux_${ARCH_TYPE}.zip" &
+# GAU installation
+install_tool "gau" "https://github.com/lc/gau/releases/download/v2.2.3/gau_2.2.3_linux_${ARCH_TYPE}.tar.gz" &
 wait
 
-# Install main script
+# Install main as-recon script
 $SUDO cp as-recon.py $BIN_DIR/as-recon
 $SUDO chmod +x $BIN_DIR/as-recon
 
-echo -e "\e[1;32m[✓] AS-RECON Pro Installed Successfully!\e[0m"
+echo -e "\e[1;32m[✓] AS-RECON Pro Fixed and Installed Successfully!\e[0m"
