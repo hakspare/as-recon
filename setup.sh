58f6a12a7a5eafc47520fe65d510c8edfb450bb5
hakspare <azizulislamshohan102@gmail.com>
2026-02-21 11:49:42 +0600

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,28 +1,36 @@
 #!/bin/bash
+
+# Colors
 G='\033[92m'
+Y='\033[93m'
 B='\033[1m'
 W='\033[0m'
 
-echo -e "${G}${B}[*] AS-RECON v10.2 Global Deployment Start...${W}"
+echo -e "${G}${B}[*] AS-RECON v10.2: Professional Installation Start...${W}"
 
-# Cleaning
-rm -rf $PREFIX/bin/as-recon
-rm -rf /usr/local/bin/as-recon
+# Removing old versions
+echo -e "${Y}[*] Cleaning old bin files...${W}"
+rm -rf $PREFIX/bin/as-recon /usr/local/bin/as-recon
 
-# Install deps
+# Installing dependencies
+echo -e "${G}[*] Installing requirements (requests, urllib3)...${W}"
 pip install requests urllib3 --quiet
 
-# Permissions & Copy
+# Setting up global command
 chmod +x as-recon.py
+
 if [ -d "$PREFIX/bin" ]; then
+    # Termux Setup
     cp as-recon.py $PREFIX/bin/as-recon
     chmod +x $PREFIX/bin/as-recon
-    echo -e "${G}[✓] Global command created in Termux!${W}"
+    echo -e "${G}[✓] Global command 'as-recon' set in Termux!${W}"
 else
+    # Linux Setup
     sudo cp as-recon.py /usr/local/bin/as-recon
     sudo chmod +x /usr/local/bin/as-recon
-    echo -e "${G}[✓] Global command created in Linux!${W}"
+    echo -e "${G}[✓] Global command 'as-recon' set in Linux!${W}"
 fi
 
 hash -r
-echo -e "\n${G}${B}Deployment Complete! Run with: as-recon -d canva.com${W}"
+echo -e "\n${B}${G}Successfully Updated to v10.2!${W}"
+echo -e "Usage: as-recon -d example.com --live"
